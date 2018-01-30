7b7a42760a2339aa462d088428ef8a3c66998094
enapupe <enapupe@gmail.com>
2018-01-29 23:56:30 -0200

    fix get-action-name util fn removing undesired sufixes

--- src/utils/get-action-name.test.js
+++ src/utils/get-action-name.test.js
@@ -6,4 +6,8 @@ test('getActionName', () => {
   expect(getActionName('SOME_MOTEREFFING_REJECTED')).toBe(BASE_ACTION_NAME)
   expect(getActionName('SOME_MOTEREFFING_FULFILLED')).toBe(BASE_ACTION_NAME)
   expect(getActionName('NOT_A_MATCH')).toBe('NOT_A_MATCH')
+  expect(getActionName('SOME_PENDING_ACTION_PENDING')).toBe('SOME_PENDING_ACTION')
+  expect(getActionName('SOME_PENDING_PENDING')).toBe('SOME_PENDING')
+  expect(getActionName('REJECT_SOMETHING_PENDING')).toBe('REJECT_SOMETHING')
+  expect(getActionName('SOME_PENDING')).toBe('SOME')
 })
